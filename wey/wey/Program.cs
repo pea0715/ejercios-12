a9ec795c9349c06b381852b9f3c834b5ee42b34f
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2020-08-18 21:36:38 -0500

    Add files via upload

---
+++ wey/wey/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace wey
+{
+    class Program
+    {
+        
+        
+            public static double factorial(int num)
+            {
+                if (num == 0 || num == 1)
+                    return 1;
+                return num * factorial(num - 1);
+            }
+            static void Main(string[] args)
+            {
+                int n;
+                Console.Write("Digite el valor del numero que desea factorial : ");
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine("El factorial de {0} es: {1}", n, factorial(n));
+                Console.ReadKey();
+            }
+
+        
+    }
+}
