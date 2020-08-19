58a2940c261e41ffb2af10b2ee40939ef89dc5db
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2020-08-18 23:17:44 -0500

    Add files via upload

---
+++ ejercio_7/ejercio_7/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ejercio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("para cada timpo de silla oprima 1 y 2 para la otra y 3 para la otra");
+            int numero = int.Parse(Console.ReadLine());
+
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("escriba numero que desea comprar de sillas");
+                    int sillas = int.Parse(Console.ReadLine());
+                    if (sillas >= 5 )
+                    {
+                        double re = sillas * 5000;
+                        double we = (re * 0.05) - sillas ;
+                        Console.WriteLine("er" + we);
+                    }
+                    else
+                    {
+                        double re = sillas * 5000;
+                        Console.WriteLine("er" + re);
+
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("escriba numero que desea comprar de sillas");
+                    int sillas1 = int.Parse(Console.ReadLine());
+                    if (sillas1 >= 5)
+                    {
+                        double re = sillas1 * 7000;
+                        double we = (re * 0.05) - sillas1;
+                        Console.WriteLine("er" + we);
+                    }
+                    else
+                    {
+                        double re = sillas1 * 5000;
+                        Console.WriteLine("er" + re);
+
+                    }
+                    break;
+
+
+                case 3:
+                    Console.WriteLine("escriba numero que desea comprar de sillas");
+                    int sillas2 = int.Parse(Console.ReadLine());
+                    if (sillas2 >= 5)
+                    {
+                        double re = sillas2 * 100000;
+                        double we = (re * 0.05) - sillas2;
+                        Console.WriteLine("er" + we);
+                    }
+                    else
+                    {
+                        double re = sillas2 * 5000;
+                        Console.WriteLine("er" + re);
+
+                    }
+                    break;
+
+
+            }
+
+        }
+    }
+}
