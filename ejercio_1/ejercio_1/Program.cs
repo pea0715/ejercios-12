4a9032ae09c10e53895ca1eb6c53dc60395cfeff
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2020-08-18 19:59:58 -0500

    Add files via upload

---
+++ ejercio_1/ejercio_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ejercio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese altura del estudiante :");
+            double tall = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el peso del estudiante :");
+            double kg = double.Parse(Console.ReadLine());
+            Console.WriteLine("oprima 1 si es hombre y 2 si es mujer : ");
+            int sexo = int.Parse(Console.ReadLine());
+
+            if (sexo == 1)
+            {
+                if (tall >= 1.60  && kg >= 150)
+                {
+                    double dosis = tall * 0.20;
+                    double qw = kg * 0.80;
+                    double final = dosis + qw;
+                    Console.WriteLine("estos son los litros que le debe poner : " + final);
+                }
+                else
+                {
+                    double dosis = tall * 0.30;
+                    double qw = kg * 0.70;
+                    double final = dosis + qw;
+                    Console.WriteLine("estos son los litros que le debe poner : " + final);
+                }
+
+            }
+            else
+            {
+                if (tall >= 1.50 && kg >= 130)
+                {
+                    double dosis = tall * 0.25;
+                    double qw = kg * 0.75;
+                    double final = dosis + qw;
+                    Console.WriteLine("estos son los litros que le debe poner : " + final);
+                }
+                else
+                {
+                    double dosis = tall * 0.35;
+                    double qw = kg * 0.65;
+                    double final = dosis + qw;
+                    Console.WriteLine("estos son los litros que le debe poner : " + final);
+                }
+
+
+            }
+        }
+    }
+}
