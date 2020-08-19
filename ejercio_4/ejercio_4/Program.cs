e2f2bb7126d99b4a294c6778693c777cb2247add
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2020-08-18 22:10:59 -0500

    Add files via upload

---
+++ ejercio_4/ejercio_4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ejercio_4
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            double suma = 0;
+            Console.WriteLine("ingrese uno si es suma , dos si es resta , 3 si es multiplicaicpon , 4 si es division : ");
+            int tiempo = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese un numero numeros : ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese un numero numeros : ");
+            double num2 = double.Parse(Console.ReadLine());
+
+
+            switch (tiempo)
+            {
+                case 1:
+                    suma = num1 + num2;
+                    Console.WriteLine(suma);
+                    break;
+
+                case 2:
+                    suma = num1 - num2;
+                    Console.WriteLine(suma);
+                    break;
+
+                case 3:
+                    suma = num1 * num2;
+                    Console.WriteLine(suma);
+
+                    break;
+
+                case 4:
+                    suma = num1 / num2;
+                    Console.WriteLine(suma);
+                    break;
+
+                default:
+                    
+                    Console.WriteLine("INGRES UN NUMERO QUE SE LE ESTA DICIENDO ");
+                    break;
+
+
+            }
+            
+        }
+
+    }
+}
