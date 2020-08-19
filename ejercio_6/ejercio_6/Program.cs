bf4b716291ef71b9a74173f94f58f7030a77cfda
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2020-08-18 22:17:33 -0500

    Add files via upload

---
+++ ejercio_6/ejercio_6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ejercio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double suma = 0;
+            Console.WriteLine("ingrese a la clase que pertene escriba 1 si es a , 2 si es b , 3 si es c : ");
+            int tiempo = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese numero de horas trabajadas : ");
+            double num1 = double.Parse(Console.ReadLine());
+
+
+            switch (tiempo)
+            {
+                case 1:
+                    suma = num1 * 7;
+                    Console.WriteLine(suma);
+                    break;
+
+                case 2:
+                    suma = num1 * 5;
+                    Console.WriteLine(suma);
+                    break;
+
+                case 3:
+                    suma = num1 * 4;
+                    Console.WriteLine(suma);
+
+                    break;
+
+                case 4:
+                    suma = num1 * 3.5;
+                    Console.WriteLine(suma);
+
+                    break;
+
+
+                default:
+
+                    Console.WriteLine("INGRES UN NUMERO QUE SE LE ESTA DICIENDO ");
+                    break;
+            }
+        }
+    }
+
+}
