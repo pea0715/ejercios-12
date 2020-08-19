4de67161d04f04dc2c8e35fce4f2767eeab31654
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2020-08-18 21:32:49 -0500

    Add files via upload

---
+++ ejercio_3/ejercio_3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ejercio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double wer = 0;
+            double er = 0; 
+            Console.WriteLine(" que tela dese compra uno o dos ;");
+            int tela = int.Parse(Console.ReadLine());
+            Console.WriteLine("años que lleva comprando en la compañia ");
+            int exp = int.Parse(Console.ReadLine());
+
+            switch (tela)
+            {
+                case 1:
+                    if (exp == 5)
+                    {
+                        Console.WriteLine("metros de tela que desea comprar :  ");
+                        double metros = double.Parse(Console.ReadLine());
+                        er = metros * 400;
+                        wer = (er * 0.8) - metros;
+                        Console.WriteLine(+wer);
+
+
+                    }
+                    if (exp == 6)
+                    {
+                        Console.WriteLine("metros de tela que desea comprar :  ");
+                        double metros = double.Parse(Console.ReadLine());
+                        er = metros * 400;
+                        wer = (er * 0.6) - metros;
+                        Console.WriteLine(+wer);
+
+
+                    }
+                    break;
+
+                case 2:
+                    if (exp == 5)
+                    {
+                        Console.WriteLine("metros de tela que desea comprar :  ");
+                        double metros = double.Parse(Console.ReadLine());
+                        er = metros * 250;
+                        wer = (er * 0.8) - metros;
+                        Console.WriteLine("cosas "+wer);
+
+
+                    }
+                    if (exp == 6)
+                    {
+                        Console.WriteLine("metros de tela que desea comprar :  ");
+                        double metros = double.Parse(Console.ReadLine());
+                        er = metros * 250;
+                        wer = (er * 0.6) - metros;
+                        Console.WriteLine(+wer);
+
+
+                    }
+                    break;
+
+
+
+            }
+
+          
+        }
+    }
+}
