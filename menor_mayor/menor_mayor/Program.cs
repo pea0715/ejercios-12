c7dd8a89ed9cec5c5a850464fbe1a4e1e0e0d741
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2020-08-19 23:15:23 -0500

    Add files via upload

---
+++ menor_mayor/menor_mayor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace menor_mayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escriba un numero :");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("escriba un numero :");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 < num2)
+            {
+                Console.WriteLine("este es el numero mayor "+num2);
+
+            }
+            else
+            {
+                Console.WriteLine("este es el numero mayor "+num1);
+            }
+        }
+    }
+}
