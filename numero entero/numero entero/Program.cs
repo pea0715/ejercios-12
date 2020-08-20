98640428c6c499d98d2195ebf9203d63faa692c6
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2020-08-19 22:06:32 -0500

    Add files via upload

---
+++ numero entero/numero entero/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace numero_entero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero : ");
+            int numero = int.Parse(Console.ReadLine());
+            double operacion = numero / 2;
+            if (operacion >= 0)
+            {
+                Console.WriteLine("numero positivo");
+            }
+            else
+                Console.WriteLine("numero negativo ");
+        }
+    }
+}
